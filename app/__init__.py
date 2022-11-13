ad44ab059ab8066d6093e0845a93801094edad06
Maya Mori <90734378+ka2-penguin@users.noreply.github.com>
2022-11-13 12:04:46 -0500

    profile page displays stories user can edit and read separately

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,13 @@ from db import *
 
 app = Flask(__name__)
 
-app.secret_key = secrets.token_bytes(32)
+# app.secret_key = secrets.token_bytes(32)
+app.secret_key = b"foo"
 
 @app.route('/')
 def home():
     if 'username' in session:
-        return render_template('profile.html') 
+        return render_template('profile.html')
     return render_template('frontpage.html')
 
 @app.route('/login')
@@ -27,7 +28,7 @@ def logout():
     return render_template('frontpage.html')
 
 @app.route('/create', methods=["POST"])
-def create_story(): 
+def create_story():
     db_table_inits()
     title = request.form['title']
     username = session['username']
@@ -44,6 +45,7 @@ def create():
 
 @app.route('/story/<story_name>', methods=["POST"])
 def add_story(story_name):
+    print("Title: "+story_name)
     username = session['username']
     text = request.form['content']
     add_to_story(story_name, username, text)
@@ -53,33 +55,36 @@ def add_story(story_name):
 @app.route('/story/<story_name>', methods=["GET"])
 def add(story_name):
     return render_template('add.html',title=story_name,last_addition=get_last_addition(story_name))
-    
+
 @app.route('/profile', methods=['GET','POST'])
 def profile():
     if request.method == 'POST':
-        if 'username' in request.form: 
+        if 'username' in request.form:
             username = request.form['username']
             password = request.form['password']
         print(request.form)
-        if 'login' in request.form: 
+        if 'login' in request.form:
             db_table_inits()
             correct_credentials = check_credentials(username, password)
-            if correct_credentials: 
+            if correct_credentials:
                 session['username'] = username
             else:
                 return render_template('login.html', error = True)
-        elif 'signup' in request.form: 
-            db_table_inits() 
+        elif 'signup' in request.form:
+            db_table_inits()
             no_user_exists = check_user_not_exists(username)
-            if no_user_exists: 
+            if no_user_exists:
                 create_new_user(username, password)
                 session['username'] = username
-            else: 
+            else:
                 return render_template('signup.html', error = True)
-    
+
     username = session['username']
-    return render_template('profile.html', username = username)
-    
+    # for story in all_stories():
+    #     print(f"{username} {story} {can_read(username,story)}")
+
+    return render_template('profile.html', username = username,readable_stories=readable_stories(username), editable_stories=editable_stories(username))
+
 @app.route('/story', methods=["POST"])
 def story():
     return render_template('create.html')
--- app/db.py
+++ app/db.py
@@ -5,14 +5,14 @@ DB_FILE = "data.db"
 db = None
 
 def db_connect():
-    global db 
+    global db
     db = sqlite3.connect(DB_FILE)
     return db.cursor()
 
 def db_close():
     db.commit()
     db.close()
-    
+
 def db_table_inits(): #creates stories and user tables if they don't exist
     c = db_connect()
     c.execute("CREATE TABLE IF NOT EXISTS stories (id int, title text)")
@@ -21,13 +21,54 @@ def db_table_inits(): #creates stories and user tables if they don't exist
     db_close()
 
 def check_story_not_exists(story):
+    c = db_connect()
+    # story_part = story + "_parts"
+    c.execute("SELECT * FROM stories")
+    print(set(c))
+    db_close()
     return True
+
+def all_stories():
+    c = db_connect()
+    # story_part = story + "_parts"
+    c.execute("SELECT * FROM stories")
+    story_list = [row[1] for row in c]
+    db_close()
+    print(story_list)
+    return story_list
+
+# returns True if user has never added to story
+def can_read(username, title):
+    contributer_table_name = title + "_contributors"
+    c = db_connect()
+    c.execute(f"SELECT * FROM {contributer_table_name}")
+    editors_list = {row[0] for row in c}
+    db_close()
+    print(editors_list)
+    return username in editors_list
+
+def readable_stories(username):
+    stories = all_stories()
+    editable_stories = []
+    readable_stories = []
+    for story in stories:
+        if can_read(username, story):
+            readable_stories.append(story)
+    return readable_stories
+
+def editable_stories(username):
+    stories = all_stories()
+    editable_stories = []
+    for story in stories:
+        if not can_read(username, story):
+            editable_stories.append(story)
+    return editable_stories
 
 def create_new_story(title, username, text):
     if check_story_not_exists(title):
         c = db_connect()
         contributer_table_name = title + "_contributors"
-        story_part = title + "_parts"        
+        story_part = title + "_parts"
         c.execute('INSERT INTO stories VALUES (?,?)',(23, title))
         c.execute(f'CREATE TABLE IF NOT EXISTS {contributer_table_name} (contributors text)')
         c.execute(f'CREATE TABLE IF NOT EXISTS {story_part} (story_part text)')
@@ -36,19 +77,25 @@ def create_new_story(title, username, text):
         db_close()
         add_to_story(title, username, text)
 
-def add_to_story(title, username, text): 
+def add_to_story(title, username, text):
     c = db_connect()
     contributer_table_name = title + "_contributors"
     story_part = title + "_parts"
+    # print(contributer_table_name)
+    # print(story_part)
+    # print(text)
+    # print(f'INSERT INTO {contributer_table_name} VALUES (\'{username}\')')
+    # print(f'INSERT INTO {story_part} VALUES (\'{text}\')')
     c.execute(f'INSERT INTO {contributer_table_name} VALUES (\'{username}\')')
     c.execute(f'INSERT INTO {story_part} VALUES (\'{text}\')')
     db_close()
     # c.execute('INSERT INTO story_info VALUES (?,?,?,?)',(23, username, ))
 
-def get_last_addition(title): 
+def get_last_addition(title):
     c = db_connect()
     story_part = title + "_parts"
-    c.execute(f"SELECT * FROM {story_part} ORDER BY story_part DESC LIMIT 1")
+    # c.execute(f"SELECT * FROM {story_part} ORDER BY story_part DESC LIMIT 1")
+    c.execute(f"SELECT * FROM {story_part}")
     print(c)
     #print(list(c))
     # print(c[-1])
